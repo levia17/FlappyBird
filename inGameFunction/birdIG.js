5cdbbb3d754fdb401e87c5fcd84c7ed127e23b2a
QuocHung <hungthanh1412hz@gmail.com>
2024-01-09 19:20:27 +0700

    screen error

--- inGameFunction/birdIG.js
+++ inGameFunction/birdIG.js
@@ -1,39 +1,43 @@
-// Declare 
-const bird = document.querySelector('.bird');
+// Declare
+const bird = document.querySelector(".bird");
 
 // Access to bird's attribute value
 const get_bird = window.getComputedStyle(bird);
 
 // Position
 // Top
-const top_bird = get_bird.getPropertyValue('top');
+const top_bird = get_bird.getPropertyValue("top");
 
 let y_bird = parseFloat(top_bird);
 // Unit
-const gravity = 3;
+const gravity = 2;
+const movement = 50;
 
-
-
-export function animation(){
-    bird.style.top = `${y_bird}px`;
-    actBird();
-    requestAnimationFrame(animation);
-}
-
+// Fix times keyup, keydown
 let count = 0;
-function actBird(){
-window.addEventListener('keypress',(button)=>{
-    count++;
-    if(count == 1){
-        if(button.key == "r"){
-            console.log('bay');
-            // y_bird -= gravity;
-            // bird.style.top = `${y_bird + gravity}px`;
-    } 
-    }
-    count = 0;
-})  
 
-
+export function animation() {
+  bird.style.top = `${y_bird}px`;
+  y_bird += gravity;
+  actBird();
+  requestAnimationFrame(animation);
 }
 
+function actBird() {
+  window.addEventListener("keypress", (button) => {
+    count++;
+    if (count == 1) {
+      if (button.key == "r") {
+        // console.log("bay");
+        y_bird -= movement;
+        // bird.style.top = `${y_bird}px`;
+      }
+    }
+  });
+  window.addEventListener("keyup", (button) => {
+    if (button.key == "r") {
+      // console.log("bay");
+      count = 0;
+    }
+  });
+}
